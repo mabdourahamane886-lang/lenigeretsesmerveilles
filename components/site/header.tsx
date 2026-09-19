e5622e3a0c7b938db62ff270899953e0ee1f0ce7
Bickriserviceagancy <254426317+mabdourahamane886-lang@users.noreply.github.com>
2026-09-19 18:06:00 +0100

    refine: move mobile search left and menu right

--- components/site/header.tsx
+++ components/site/header.tsx
@@ -49,38 +49,11 @@ export default async function Header() {
     <>
       <header className="siteHeader">
         <div className="container siteHeaderInner">
-          <details className="siteMobileMenu">
-            <summary aria-label="Ouvrir le menu principal"><Menu size={20} /></summary>
-            <div className="siteMobilePanel">
-              <div className="siteMobileHead">
-                <span>Navigation</span>
-                <span className="siteMiniFlag">🇳🇪</span>
-              </div>
-              <nav aria-label="Navigation mobile">
-                {mobileItems.map(({ label, href, icon: Icon }) => (
-                  <Link href={href} key={href}><Icon size={17} /><span>{label}</span></Link>
-                ))}
-                <Link href="/articles"><BookOpenText size={17} /><span>Articles</span></Link>
-                <Link href="/carte"><Compass size={17} /><span>Carte</span></Link>
-                <Link href="/recherche"><Search size={17} /><span>Recherche</span></Link>
-                <Link className="siteMobileCta" href="/contribution"><Send size={17} /><span>Contribuer au projet</span></Link>
-                {isAdmin && (
-                  <>
-                    <div className="siteMobileAdminDivider" aria-hidden="true" />
-                    <Link className="siteMobileAdminLink" href="/admin">
-                      <ShieldCheck size={17} /><span>Administration</span>
-                    </Link>
-                    <Link className="siteMobileAdminPublish" href="/admin/evenements">
-                      <PlusCircle size={17} /><span>Publier un événement</span>
-                    </Link>
-                    <Link className="siteMobileAdminPublish" href="/admin/articles">
-                      <PlusCircle size={17} /><span>Publier un article</span>
-                    </Link>
-                  </>
-                )}
-              </nav>
-            </div>
-          </details>
+          <div className="siteHeaderMobileLeft">
+            <Link className="siteSearchButton" href="/recherche" aria-label="Rechercher sur le site">
+              <Search size={18} />
+            </Link>
+          </div>
 
           <Link className="siteBrand" href="/" aria-label="Le Niger et ses Merveilles, accueil">
             <span className="siteBrandMark" aria-hidden="true">
@@ -106,7 +79,7 @@ export default async function Header() {
           </nav>
 
           <div className="siteHeaderActions">
-            <Link className="siteSearchButton" href="/recherche" aria-label="Rechercher sur le site">
+            <Link className="siteSearchButton siteDesktopSearch" href="/recherche" aria-label="Rechercher sur le site">
               <Search size={18} />
             </Link>
             <Link className="siteContributeButton" href="/contribution">
@@ -114,7 +87,38 @@ export default async function Header() {
               Contribuer
             </Link>
 
-
+            <details className="siteMobileMenu">
+              <summary aria-label="Ouvrir le menu principal"><Menu size={20} /></summary>
+              <div className="siteMobilePanel">
+                <div className="siteMobileHead">
+                  <span>Navigation</span>
+                  <span className="siteMiniFlag">🇳🇪</span>
+                </div>
+                <nav aria-label="Navigation mobile">
+                  {mobileItems.map(({ label, href, icon: Icon }) => (
+                    <Link href={href} key={href}><Icon size={17} /><span>{label}</span></Link>
+                  ))}
+                  <Link href="/articles"><BookOpenText size={17} /><span>Articles</span></Link>
+                  <Link href="/carte"><Compass size={17} /><span>Carte</span></Link>
+                  <Link href="/recherche"><Search size={17} /><span>Recherche</span></Link>
+                  <Link className="siteMobileCta" href="/contribution"><Send size={17} /><span>Contribuer au projet</span></Link>
+                  {isAdmin && (
+                    <>
+                      <div className="siteMobileAdminDivider" aria-hidden="true" />
+                      <Link className="siteMobileAdminLink" href="/admin">
+                        <ShieldCheck size={17} /><span>Administration</span>
+                      </Link>
+                      <Link className="siteMobileAdminPublish" href="/admin/evenements">
+                        <PlusCircle size={17} /><span>Publier un événement</span>
+                      </Link>
+                      <Link className="siteMobileAdminPublish" href="/admin/articles">
+                        <PlusCircle size={17} /><span>Publier un article</span>
+                      </Link>
+                    </>
+                  )}
+                </nav>
+              </div>
+            </details>
           </div>
         </div>
       </header>
